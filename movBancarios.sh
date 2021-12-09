93ff5debe578ec9bc845cf91c587de01f503a7a4
Mario Silla Olmos <silolm.mail@gmail.com>
2021-12-09 21:59:45 +0100

    prueba de errores 2.3

--- movBancarios.sh
+++ movBancarios.sh
@@ -75,21 +75,21 @@ function newMove() {
 	#								DÍA
 	#--------------------------------------------------------------------
 
-	trueDay="true"
+	trueDay=0
 	#Pedimos el dia /mes /año
     read -r -p "Dia: " dia
 	#Hacemos el control de errores y hacemos la comprobacion
     compro=`expr $dia \* 1 2> /dev/null`
 
-	while [ $trueDay = "true" ]; do
+	while [ $trueDay=0 ]; do
 		if [ $? -ne 0 ]; then
 			echo "[1] --> El dia introducido no es correcto"
         	read -r -p "Día: " dia
         	compro=`exrp $dia \* 1 2> /dev/null`
 		else
-			if [ $dia -ge 1 -o $dia -le 31 -a $trueDay = "true" ]; then
-				$trueDay="false";
-				echo "JODER"
+			if [ $dia -ge 1 -o $dia -le 31 ]; then
+				$trueDay=1;
+				break
 			else
 				if [ $? -ne 0 ]; then
 					echo "[2] --> El dia introducido no es correcto"
